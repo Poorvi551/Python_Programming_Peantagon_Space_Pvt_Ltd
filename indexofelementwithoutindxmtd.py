36042fcb9059e6c6c1f0ae46c792b56a5478a0fb
S B Poorvi <182614650+Poorvi551@users.noreply.github.com>
2026-06-30 10:14:00 +0530

    Update indexofelementwithoutindxmtd.py

--- indexofelementwithoutindxmtd.py
+++ indexofelementwithoutindxmtd.py
@@ -1,6 +1,17 @@
 #wap to print index values of the given element without using index values. - assignment q2
-def find_index_without_idxmetd(l):
-    for i in enumerate(l):                      # used to print the index values of an element
-        print(i)
+
 l=eval(input("Enter list:"))
-find_index_without_idxmetd(l)
+val=int(input("Enter val:"))
+for i in range(len(l)):
+    if l[i]==val:
+        print(i)
+        break
+
+# def find_index_without_idxmetd(l,val):
+#     for i in range(len(l)):
+#         if l[i]==val:
+#             print(i)
+#             break
+# l=eval(input("Enter list:"))
+# val=int(input("Enter val:"))
+# find_index_without_idxmetd(l,val)
